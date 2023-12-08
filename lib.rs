8252856aecfefca443df7a6d5c50f3c2221d3ff7
Maciek Malik <poczta@maciekmalik.pl>
2023-12-08 10:05:12 +0100

    invalid code #1

---
+++ lib.rs
@@ -0,0 +1,36 @@
+#![cfg_attr(not(feature = "std"), no_std, no_main)]
+
+pub use psp22::{PSP22Data, PSP22Event};
+pub use psp22::PSP22;
+pub use psp22::PSP22Error;
+pub use self::token::Token;
+
+/// PSP22 token implementation.
+///
+/// This struct represents a PSP22 compliant fungible token.
+#[ink::contract]
+pub mod token {
+    use psp22::{PSP22, PSP22Data};
+
+    #[ink(storage)]
+    pub struct Token {
+        pub data: PSP22Data,
+    }
+
+    impl Token {
+
+        #[ink(constructor)]
+        pub fn new(
+            supply: u128,
+        ) -> Self {
+            Self {
+                data: PSP22Data::new(supply, Self::env().caller()),
+            }
+        }
+
+        #[ink(message)]
+        pub fn total_supply(&self) -> u128 {
+            self.data.total_supply()
+        }
+    }
+}
